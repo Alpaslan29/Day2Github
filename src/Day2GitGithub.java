0cf6f0d3d1ba239f4e4f49877e2ee77481c2d1c1
Aslan Kocagöz <alpaslankocagoz@gmail.com>
2022-08-15 21:16:43 +0300

    V2

--- src/Day2GitGithub.java
+++ src/Day2GitGithub.java
@@ -18,5 +18,8 @@ NOT: Yukarıdaki iki komutu tek seferde kullandıktan sonra ikinci commit'lerim 
 kullanırız
 
   */
+    public static void main(String[] args) {
+        System.out.println("Github'ı da ogrendim");
+    }
 
 }
